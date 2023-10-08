bb2271b454f617ccaa5731b41103c07a694c638a
noobslearn <138974329+SupaMega24@users.noreply.github.com>
2023-10-09 00:15:25 +0900

    Add event listeners for pixelated trail

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const container = document.getElementById('container');
+    let isDrawing = false;
 
     const numberOfRows = 16;
     const numberOfCols = 16;
@@ -8,6 +9,20 @@ document.addEventListener('DOMContentLoaded', function () {
         const div = document.createElement('div');
         div.classList.add('grid-item');
         container.appendChild(div);
+
+        div.addEventListener('mouseover', function (event) {
+            if (isDrawing) {
+                event.target.style.backgroundColor = '#3498db';
+            }
+        });
     }
+
+    document.addEventListener('mousedown', function () {
+        isDrawing = true;
+    });
+
+    document.addEventListener('mouseup', function () {
+        isDrawing = false;
+    });
 });
 
